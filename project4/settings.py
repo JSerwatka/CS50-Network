2ae85b774d3b00e1cfdf5f35557496f0d77674b0
Jakub Serwatka <kubaserw@gmail.com>
2020-12-09 15:27:37 +0100

    github .yml env

--- project4/settings.py
+++ project4/settings.py
@@ -147,7 +147,7 @@ LOCALE_PATHS = (os.path.join(BASE_DIR, 'locale'),)
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 # STATICFILES_DIRS = [os.path.join(BASE_DIR, "static"),]
 
