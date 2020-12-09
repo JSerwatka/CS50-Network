83dfb34edee4255922e9d169ef98517985458ccf
Jakub Serwatka <kubaserw@gmail.com>
2020-12-09 11:46:17 +0100

    fix: no db defined

--- project4/settings.py
+++ project4/settings.py
@@ -88,14 +88,14 @@ WSGI_APPLICATION = 'project4.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-if 'test' in sys.argv:
+
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.sqlite3',
             'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
         }
     }
-else:
+if 'test' not in sys.argv:
     db_from_env = dj_database_url.config(conn_max_age=600)
     DATABASES['default'].update(db_from_env)
 
