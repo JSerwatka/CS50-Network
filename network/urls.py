42a1d13db277ac50db56af79d640a24e6b5bf0f2
Jakub Serwatka <kubaserw@gmail.com>
2020-12-10 12:15:18 +0100

    change: media static file only in debug mode

--- network/urls.py
+++ network/urls.py
@@ -18,4 +18,5 @@ urlpatterns = [
     path("register", views.register, name="register"),
 ]
 
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
