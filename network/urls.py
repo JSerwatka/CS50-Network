e098a7c38eed62a0d763ab6b5fe7a5b158165b90
Jakub Serwatka <kubaserw@gmail.com>
2020-12-10 12:33:08 +0100

    delete: media file

--- network/urls.py
+++ network/urls.py
@@ -17,6 +17,3 @@ urlpatterns = [
     path("logout", views.logout_view, name="logout"),
     path("register", views.register, name="register"),
 ]
-
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
